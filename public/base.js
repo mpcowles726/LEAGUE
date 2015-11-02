ef4fe076691a244b635bce55b976d3b03bcdf25b
HollisDayCheer <hollisj@carleton.edu>
2015-11-02 11:37:15 -0800

    fixed the hide and show

--- public/base.js
+++ public/base.js
@@ -1,24 +1,35 @@
 
 $(document).ready(function() {
 console.log('clientside js connected');
-var franklin = ('<% include (./partials/Franklin) %>');
+$(".partials").children().hide();
+
+$("#playerList").children().on("click", function(e){
+	e.preventDefault();
+	console.log("help");
+	$(".partials").children().hide();
+	console.log(this);
+	var classString = ".partials ." + $(this).attr('class');
+	console.log(classString);
+	$(classString).show();
+
+	//$(".Marcus").show();
+//	$(classString).show();
+})
+// var franklin = ('<% include (./partials/Franklin) %>');
 	
-$("#Franklin").click(function () {
-	$("#partials").append(franklin);
+// $("#Franklin").click(function () {
+// 	$("#partials").append(franklin);
 
 	
 	
+// });
+	
+	
+
+
+
+
+
+
+
 });
-	
-	
-
-
-
-
-
-
-
-});
-
-
-ejs.render(string, options);
